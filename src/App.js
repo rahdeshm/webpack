e286a93b6315a9de0049ee221de2cf5a647ba9a3
Rahul Deshmukh <rahdeshm@WKMZT18D8005.global.publicisgroupe.net>
2023-02-14 15:20:49 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <About/>
     </div>
   );
 }
